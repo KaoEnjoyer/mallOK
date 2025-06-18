2258759ef6bf6c92b2d0cf211c9386c58291a170
KaoEnjoyer <ludwichowskiwiktor@gmail.com>
2025-06-19 00:37:03 +0200

    update

--- mallok.c
+++ mallok.c
@@ -13,17 +13,15 @@
 struct _heap_type;
 struct _heap_chunk;
 
-//size_t _align_memory
+// size_t _align_memory
 
 size_t get_page_size() { return sysconf(_SC_PAGE_SIZE); }
 int _init_heap(struct _heap_type *heap, size_t size);
 
-struct _heap_type _head;
-
 struct _heap_chunk {
   short is_used;
   size_t size;
-  void* ptr;//ptr o chunk memory
+  void *ptr; // ptr to chunk memory
   struct _heap_chunk *next;
 };
 
@@ -32,23 +30,37 @@ struct _heap_type {
   struct _heap_chunk *start;
 };
 
+struct _heap_type _head;
+
+struct _heap_chunk *_find_chunk(void *ptr_to_memory) {
+  struct _heap_chunk *current = _head.start;
+  while (current != NULL) {
+    if (current->ptr == ptr_to_memory)
+      return current;
+    current = current->next;
+  }
+  return NULL;
+}
+
 void *mallOK(size_t size) {
   if (_head.start == NULL) {
     _init_heap(&_head, size);
   }
 
   struct _heap_chunk *toReturn = _head.start;
-  while(toReturn->is_used || toReturn->size < size){
+
+  while (toReturn->is_used || toReturn->size < size) {
     toReturn = toReturn->next;
-      if(toReturn == NULL)break;
+    if (toReturn == NULL)
+      break;
   }
+
   if (toReturn == NULL) {
     fprintf(stderr, "MALLOK COULD NOT FIND FREE CHUNK %u , %u \n",
             _head.start->is_used, _head.start->size);
     return MALLOK_FAILED;
   }
-
-  
+//https://stackoverflow.com/questions/8752546/how-does-malloc-understand-alignment
   size_t offset = size;
   if (_head.start->next == NULL) {
     _head.start = (void *)((char *)_head.start + size);
@@ -78,18 +90,16 @@ int _init_heap(struct _heap_type *heap, size_t size) {
   chunk->size = get_page_size() - sizeof(struct _heap_chunk);
   chunk->is_used = 0;
   chunk->next = NULL;
-  chunk->ptr = chunk+1;
+  chunk->ptr = chunk + 1;
   heap->start = chunk;
   heap->empty_chunks = 1;
   return GOOD_ALLOC;
 }
 
-
-void MallNotOk(void* ptr){
-//dealocation of memory (like free, delete)
-  printf("idk czy to tak działa %d",sizeof(ptr));
+void MallNotOk(void *ptr) {
+  // dealocation of memory (like free, delete)
+  printf("idk czy to tak działa %d", sizeof(ptr));
 }
-
 
 struct test {
   int a;
@@ -110,10 +120,8 @@ int main(int argc, char *argv[]) {
   T->a = 2;
   T->b = 33;
   T->c = 'c';
-  
-    
+
   printf("struct T a: %d , b: %d , c: %c , addres: %p", T->a, T->b, T->c, T);
-    printf("{%d}",sizeof(T));
+  printf("{%d}", sizeof(T));
   MallNotOk(T);
 }
-
